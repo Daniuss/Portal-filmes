a0f0ad3de463defe4dd80243ba05cf14aa177202
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 20:59:58 -0300

    Meus Filmes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header(){
     return(
         <>
-            <header>
+            <header className="w-full h-10 flex justify-between items-center p-10 mb-10 bg-purple">
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Filmes</a></li>
-                        <li><a href="">Gêneros</a></li>
-                    </ul>
+                    <ul className="flex gap-5">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/movies">Filmes</NavLink></li>
+                        <li><NavLink to="/genre">Gênero</NavLink></li>
+                   </ul>
                 </nav>
             </header>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,29 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {path: '/movies', element: <MovieListPage />},
+      {path: '/movies/:id', element: <MovieDetailPate />},
+      {path: '/genre', element: <GenreList />},
+      {path: '/genre/:id', element: <MovieByGenrePage />},
+      {path: '*', element: <PageNotFound />}
+    ] 
+  }
+
+]
+
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
+
